e4a1f6a2251b0a3b6978f3d17c66a9d131c84a9d
Ifeanyichukwu John <ifeanyichukwujohn70@gmail.com>
2021-09-28 23:55:08 +0100

    hello world

--- views/home.php
+++ views/home.php
@@ -1,2 +1,16 @@
-<?php $this->title = "John Code MVC design";?>
-<h1>Welcome <?php echo $name ?></h1>
+<?php $this->title = "John Code MVC design"; ?>
+<div class="container">
+    <div class="row" style="border:2px solid red;">
+        <div class="col">
+            <div class="img">
+                <img class="img-responsive round" style="border-radius: 50%;height:150px;width:150px;" src="img/me.jpg" alt="my image">
+            </div>
+        </div>
+        <div class="col">
+            <h2>Project Details</h2>
+            <h4>Name: John Code MVC Design</h4>
+            <h4>Built: Ifeanyichukw John</h4>
+        </div>
+    </div>
+    <h1>Welcome <?php echo $name ?></h1>
+</div>
